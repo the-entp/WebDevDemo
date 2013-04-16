5395f603e6e05bc7746a7bd66118eb7c0b7b4266
= <annashi.ys13@gmail.com>
2013-04-16 01:33:51 -0500

    Created 2 window div

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@ $(
 					"<tr><td>John</td><td>Smith</td></tr>" +
 					"<tr><td>Edward</td><td>Smith</td></tr>"+
 					"<tr><td>Another</td><td>Name</td></tr></table>";
+		var paragraph = "<textarea rows='4' cols='50'></textarea>";
 
-		$('.main').html(table);
+		$('#data').html(table);
+		$('#description').html(paragraph);
+		$('#data tbody tr:odd').css({'background-color':'#dddddd', 'color':'#666666'});
+		$('#data td').click(
+			function() {
+				$('#description textarea').text($('#description textarea').text() + ' ' + $(this).text());
+			}
+		);
 	}
 	
 );
