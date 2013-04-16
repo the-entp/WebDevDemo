e9df23a1b4dc8ab8ce974dc215887fb2e2bbb3cb
= <annashi.ys13@gmail.com>
2013-04-16 01:02:10 -0500

    Created a sample table

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(
+	function() {
+		var table = "<table border='1'><th>First name</th><th>Last name</th>" +
+					"<tr><td>John</td><td>Smith</td></tr>" +
+					"<tr><td>Edward</td><td>Smith</td></tr>"+
+					"<tr><td>Another</td><td>Name</td></tr></table>";
+
+		$('.main').html(table);
+	}
+	
+);
+	
+
+
+
+
